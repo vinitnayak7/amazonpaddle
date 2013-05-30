70f687cbc20fc5448cd131591a41b0ea1e6c4188
Lin <raymolin@406c8f0c0ac4.ant.amazon.com>
2013-05-30 14:50:09 -0700

    Added Menu/Registration Layouts

--- android/gen/com/amazon/paddle/R.java
+++ android/gen/com/amazon/paddle/R.java
@@ -24,18 +24,44 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int TextViewEmail=0x7f080004;
+        public static final int TextViewPassword=0x7f080005;
+        public static final int TextViewUser=0x7f080003;
+        public static final int action_settings=0x7f080009;
+        public static final int passwordID=0x7f080002;
+        public static final int rEmailID=0x7f080007;
+        public static final int rPasswordID=0x7f080008;
+        public static final int rUsernameID=0x7f080006;
+        public static final int textView2=0x7f080001;
+        public static final int usernameID=0x7f080000;
     }
     public static final class layout {
         public static final int activity_menu=0x7f030000;
+        public static final int activity_registration=0x7f030001;
     }
     public static final class menu {
         public static final int menu=0x7f070000;
+        public static final int registration=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int id=0x7f050007;
+        public static final int passwordID=0x7f050006;
+        public static final int pw=0x7f050008;
+        public static final int rEmail=0x7f05000e;
+        public static final int rEmailID=0x7f050011;
+        public static final int rPassword=0x7f05000d;
+        public static final int rPasswordID=0x7f050010;
+        public static final int rUsername=0x7f05000c;
+        public static final int rUsernameID=0x7f05000f;
+        public static final int register=0x7f05000a;
+        public static final int registerID=0x7f05000b;
+        public static final int title_activity_registration=0x7f050009;
+        public static final int userExample=0x7f050004;
+        public static final int username=0x7f050003;
+        public static final int usernameID=0x7f050005;
     }
     public static final class style {
         /** 
