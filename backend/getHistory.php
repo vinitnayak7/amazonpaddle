ad516494b74e464f1a0af452536cb856de780301
Nayak, Vinit <vinitnayak87@berkeley.edu>
2013-05-31 14:11:35 -0700

    Updated History and getChalleneges API.

--- backend/getHistory.php
+++ backend/getHistory.php
@@ -7,14 +7,34 @@ if (mysqli_connect_errno($con))
 
 $userID = $_REQUEST['id'];
 
-$query = 'SELECT * FROM games WHERE p2='.$userID.' AND winner>0 ORDER BY modified';
+$query = 'SELECT * FROM games WHERE (p2='.$userID.' OR p1='.$userID.') AND winner > 0 ORDER BY modified';
 $response = mysqli_query($con, $query);
 
 if ($response) {
 	$totalRows = mysqli_num_rows($response);
 	$count = 1;
 	while ($row = mysqli_fetch_assoc($response)) {
-		print($row['name'].','.$row['p1'].','.$row['id']."\n");
+	    print($row['name'].',');
+	    $ch=curl_init('http://brandonnalls.com/paddle/getNameFromID.php?id='.$row['p2']);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    $opponent2=curl_exec($ch);
+	    curl_close($ch);
+	    $ch=curl_init('http://brandonnalls.com/paddle/getNameFromID.php?id='.$row['p1']);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    $opponent1=curl_exec($ch);
+	    curl_close($ch);
+	    if ($row['p1'] == $userID) {
+	    	print($opponent2.',');
+	    } else {
+	    	print($opponent1.',');
+	    }
+	    if ($row['winner'] == $row['p1']) {
+	    	print($opponent2.',');
+	    } else {
+	    	print($opponent1.',');
+	    }
+	    print($row['winnerScore'].','.$row['loserScore']."\n");
+		
 	}
 } else {
 	print('failure');
