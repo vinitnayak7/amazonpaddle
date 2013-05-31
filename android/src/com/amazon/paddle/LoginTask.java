8139fed3e4a7dae8ae29095637be15813ca7574d
Tim Stephani <timmy.stephani@gmail.com>
2013-05-31 09:18:35 -0700

    login working crappily

---
+++ android/src/com/amazon/paddle/LoginTask.java
@@ -0,0 +1,12 @@
+package com.amazon.paddle;
+
+import java.net.URLEncoder;
+
+import com.amazon.paddle.credential.User;
+import com.amazon.paddle.global.Global;
+import com.amazon.paddle.web.WebRequest;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+
